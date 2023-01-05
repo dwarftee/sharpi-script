ed049dcc830b10590db0df1eaab2dbff878f2c71
sharpi <64mrnoah@gmail.com>
2023-01-05 18:19:21 +0000

    turned it into a function

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-class a{
-    constructor(code) {
-        this.code = code;
-    }
-    run()  {
-        console.log(this.code);
-    }
-}
-
-
-
-//code that runs
-
-const fs = require('fs');
-
-fs.readFile('main.sharpi', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
+function compile() {
+  class a{
+      constructor(code) {
+          this.code = code;
+      }
+      run()  {
+          console.log(this.code);
+      }
   }
-  var code = data;
-  const c = new a(code);
-  c.run();
-});
+
+
+
+  //code that runs
+
+  const fs = require('fs');
+
+  fs.readFile('main.sharpi', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    var code = data;
+    const c = new a(code);
+    c.run();
+  });
+}
+compile();
