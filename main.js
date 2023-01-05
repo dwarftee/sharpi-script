fb5d61431a795ae0f8800170bef71e6895fbce8b
sharpi <64mrnoah@gmail.com>
2023-01-05 18:00:32 +0000

    commit :)

---
+++ main.js
@@ -0,0 +1,20 @@
+class a{
+    constructor(code) {
+        this.code = code;
+    }
+    run()  {
+        console.log(this.code);
+    }
+}
+
+const fs = require('fs');
+
+fs.readFile('main.sharpi', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  var code = data;
+  const c = new a(code);
+  c.run();
+});
